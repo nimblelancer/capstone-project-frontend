a70f225b7fea66f0289f76ad20028e1330f0d32b
ThinhGH <thinhkng@gmail.com>
2024-03-16 15:37:00 +0700

    new sign in and sign up

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,17 +1,102 @@
-import React from 'react';
-import SignUpForm from '@/components/SignUpForm/signupForm'; 
-import styles from './page.module.css'; 
+'use client';
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link"; // Import Link from next/link
+import styles from "./page.module.css";
 
-const Page = () => {
+const Signup = () => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleChange = ({ currentTarget: input }) => {
+    setData({ ...data, [input.name]: input.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = "http://localhost:8080/api/users";
+      const { data: res } = await axios.post(url, data);
+      console.log(res.message);
+      // Chuyển hướng đến trang "/login" bằng cách sử dụng Link từ next/link
+      <Link href="/signIn">
+        <a>Sign in</a>
+      </Link>;
+    } catch (error) {
+      if (
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status <= 500
+      ) {
+        setError(error.response.data.message);
+      }
+    }
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.centeredContent}>
-        <div className={styles.formWrapper}>
-          <SignUpForm />
+    <div className={styles.signup_container}>
+      <div className={styles.signup_form_container}>
+        <div className={styles.left}>
+          <h1>Welcome Back</h1>
+          <Link href="/signIn">
+          <button type="button" className={styles.white_btn}>
+							Sign In
+						</button>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <form className={styles.form_container} onSubmit={handleSubmit}>
+            <h1>Create Account</h1>
+            <input
+              type="text"
+              placeholder="First Name"
+              name="firstName"
+              onChange={handleChange}
+              value={data.firstName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              name="lastName"
+              onChange={handleChange}
+              value={data.lastName}
+              required
+              className={styles.input}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={data.email}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+              value={data.password}
+              required
+              className={styles.input}
+            />
+            {error && <div className={styles.error_msg}>{error}</div>}
+            <button type="submit" className={styles.green_btn}>
+              Sign Up
+            </button>
+          </form>
         </div>
       </div>
     </div>
   );
 };
 
-export default Page;
+export default Signup;
