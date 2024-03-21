5231b8883939658c0f40291f031cca58b27835ab
tranviettuantvt <tranviettuantvt@gmail.com>
2024-03-22 02:10:26 +0700

    ::

--- components/SigninForm/signin.tsx
+++ components/SigninForm/signin.tsx
@@ -1,25 +1,25 @@
-import { useState } from 'react';
-import axios from 'axios';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import { createTheme } from '@mui/material/styles';
+import { useState } from "react";
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 
 const defaultTheme = createTheme();
 
 export default function SignIn() {
   const [formData, setFormData] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
     rememberMe: false,
   });
   const [error, setError] = useState(null);
@@ -27,19 +27,24 @@ export default function SignIn() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3055/api/user/signin', formData);
-      const { token, user } = response.data;
+      const response = await axios.post(
+        "http://localhost:3055/api/user/signin",
+        formData,
+      );
+      const { user } = response.data;
+
+      console.log(user);
 
       // Store token in local storage
-      localStorage.setItem('token', token);
+      localStorage.setItem("user", JSON.stringify(user));
       if (response.status === 200) {
-        window.location.href = '/personalHealth';
+        window.location.href = "/personalHealth";
       } else {
-        setError('Đăng nhập không thành công. Vui lòng thử lại sau.');
+        setError("Đăng nhập không thành công. Vui lòng thử lại sau.");
       }
     } catch (error) {
-      setError('Đã xảy ra lỗi khi đăng nhập. Vui lòng thử lại sau.');
-      console.error('Đã xảy ra lỗi khi đăng nhập:', error);
+      setError("Đã xảy ra lỗi khi đăng nhập. Vui lòng thử lại sau.");
+      console.error("Đã xảy ra lỗi khi đăng nhập:", error);
     }
   };
 
@@ -47,13 +52,13 @@ export default function SignIn() {
     const { name, value, checked, type } = event.target;
     setFormData((prevState) => ({
       ...prevState,
-      [name]: type === 'checkbox' ? checked : value,
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
         <Grid
           item
@@ -61,28 +66,33 @@ export default function SignIn() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-            backgroundRepeat: 'no-repeat',
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
-              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
           }}
         />
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <Box sx={{ mt: 10, textAlign: 'center' }}>
-            <Avatar sx={{ bgcolor: 'secondary.main', marginTop: '90px', m: 'auto' }}>
+          <Box sx={{ mt: 10, textAlign: "center" }}>
+            <Avatar
+              sx={{ bgcolor: "secondary.main", marginTop: "90px", m: "auto" }}
+            >
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5" sx={{ mt: 2 }}>
               Sign in
             </Typography>
-            {error && <Typography sx={{ color: 'red' }}>{error}</Typography>}
+            {error && <Typography sx={{ color: "red" }}>{error}</Typography>}
             <Box
               component="form"
               noValidate
               onSubmit={handleSubmit}
-              sx={{ mt: 1, mr:10 , ml: 10 }}
+              sx={{ mt: 1, mr: 10, ml: 10 }}
             >
               <TextField
                 margin="normal"
